41289898a627a29f4525eff6fee7b424151ae2f7
GPC Thushani <charithathushani2003@gmail.com>
2026-03-07 22:51:56 +0530

    Correct incident controller methods and error handling
    Fix errors in incident controller methods and improve error handling.

--- backend/controllers/incidentContoller.js
+++ backend/controllers/incidentContoller.js
@@ -1,1 +1,52 @@
+//Handles creating reports with GPS data and manages the lifecycle status.
+const Incident = require('../models/Incident.js');
 
+//create new incident report
+exports.createIncident = async (req, res) => { 
+  try{
+    const{ type, description, location} = req.body;
+    const newIncident = new Incident({
+      user_id: req.user.id,
+      type,
+      description,
+      location,
+      latitude,
+      longitude
+    });
+
+  const savedIncident = await newIncident.save();
+  res.status(201).json(savedIncident);
+}
+  catch(err) {
+    res.status(500).json({ message: Error creating incident", error: err.message });
+  } 
+};
+
+
+//get all incidents - for live map
+exports.getAllIncidents = async (req, res) => {
+  try{
+    const incidents = await Incidents.find().sort({ timestamp: -1 });
+    res.status(200).json(incident);
+  } 
+  catch(err) {
+    res.status(500).json({ message: "Error fetching incidents", error: err.message });
+  }
+};
+
+
+//update incident status - admin, responder
+exports.updateIncidentStatus = async (req, res) => {
+  try{
+    const {status} = req.body;
+    const updateIncident = await Incident.findByIdAndUpdate(
+      req.params.id,
+      { ststus },
+      { new: true }
+      );
+    res.status(200).json(updateIncident);
+  }
+  catch(err) {
+    res.status(500).json({ messaage: "Error updating status", error: err.message });
+  }
+};
