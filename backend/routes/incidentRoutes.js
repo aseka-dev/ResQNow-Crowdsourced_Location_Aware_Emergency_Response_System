540a6a36af6cc816ffc15ddee58683b415adf580
GPC Thushani <charithathushani2003@gmail.com>
2026-03-15 16:09:27 +0530

    Add image upload middleware to incident creation

--- backend/routes/incidentRoutes.js
+++ backend/routes/incidentRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const incidentController = require('../controllers/incidentController');
-const { verifyToken } = require('../middleware/authMiddleware'); 
+const { verifyToken } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware'); 
 
 router.get('/', incidentController.getAllIncidents);
 
-router.post('/', verifyToken, incidentController.createIncident);
+router.post(
+  '/',
+  verifyToken,
+  upload.single("image"),   =
+  incidentController.createIncident
+);
+
 router.put('/:id/status', verifyToken, incidentController.updateIncidentStatus);
 
 module.exports = router;
