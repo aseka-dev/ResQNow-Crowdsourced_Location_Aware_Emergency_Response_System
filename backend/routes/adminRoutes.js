fbdb42a10797b4625825ee2e5264cb9b00cf44ea
GPC Thushani <charithathushani2003@gmail.com>
2026-04-03 19:05:35 +0530

    Add route for admin to verify responder

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,4 +1,9 @@
+const express = require("express")
+const router = express.Router();
 const adminController = require("../controllers/adminController");
+
+const { verifyToken } = require("../middleware/authMiddleware");
+const allowRoles = require("../middleware/roleMiddleware");
 
 // Admin verifies responder
 router.put(
@@ -7,3 +12,5 @@ router.put(
   allowRoles("Admin"),
   adminController.verifyResponder
 );
+
+module.exports = router;
