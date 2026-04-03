9da940c4800c21e42f287382e6c42fb2355f1c87
KavinduAnj <wkanjana1590@gmail.com>
2026-04-03 19:07:17 +0530

    fixed errors

--- mobile-app/src/navigation/AppNavigator.js
+++ mobile-app/src/navigation/AppNavigator.js
@@ -2,8 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/Register1"; // Assuming Register1 is the first step of registration
 
 const Stack = createNativeStackNavigator();
 
