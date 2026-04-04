181c0918e240bf62bb19e3febce0a4f782a67f46
DThawi <dthawi0319@gmail.com>
2026-04-04 12:55:43 +0530

    style: update shared styles for common screens

--- mobile-app/src/screens/auth/Styles/commonStyles.js
+++ mobile-app/src/screens/auth/Styles/commonStyles.js
@@ -4,17 +4,35 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
-        padding: 20,
+        padding: 10,
     },
     title: {
-        fontSize:22,
-        fontWeight: "bold",
+        fontSize:18,
+        fontWeight: 600,
         marginBottom: 20,
+        lineHeight: 18,
+
     },
-    text:{
-        fontSize: 16,
-        lineHeight,
-        color: "#333",
+    textHeader: {
+        color: "#003049",
+        fontSize: 18,
+        fontWeight: 600,
+        lineHeight: 28,
+        marginBottom: 5,
+    },
+
+    textSubHeader: {
+        color: "#2B2D42",
+        fontSize: 14,
+        fontWeight: 700,
+        lineHeight: 20,
+    },
+
+    textContent:{
+        fontSize: 14,
+        lineHeight: 20,
+        color: "#2B2D42",
+        fontWeight: 400,
         marginBottom: 10,
 
     },
